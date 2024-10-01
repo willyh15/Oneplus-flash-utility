3e918557de283694e0cd7b450f88774ece3e56d0
Will Harrison <willharrison2016@gmail.com>
2024-10-01 02:42:07 -0500

    Update environment.py

---
+++ environment.py
@@ -0,0 +1,17 @@
+import os
+
+class Environment:
+    """
+    Manages the application's environment variables, paths, and sensitive data.
+    """
+    @staticmethod
+    def get_env_var(variable_name, default=None):
+        return os.getenv(variable_name, default)
+
+    @staticmethod
+    def set_env_var(variable_name, value):
+        os.environ[variable_name] = value
+
+    @staticmethod
+    def list_all_env_vars():
+        return dict(os.environ)
